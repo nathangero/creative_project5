cf0c14a6f78e8572ba4c2cfadc1d6488cdc0b492
Nathan Geronimo <nathangero@yahoo.com>
2018-04-11 13:55:24 -0600

    Added homepage; registration works; work on login and past login

---
+++ src/store.js
@@ -0,0 +1,98 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        user: {},
+        loggedIn: false,
+        loginError: '',
+        registerError: '',
+        list: [],
+    },
+    getters: {
+        user: state => state.user,
+        loggedIn: state => state.loggedIn,
+        loginError: state => state.loginError,
+        registerError: state => state.registerError,
+        list: state => state.list,
+    },
+    mutations: {
+        setUser(state, user) {
+            state.user = user;
+        },
+        setLogin(state, status) {
+            state.loggedIn = status;
+        },
+        setLoginError(state, message) {
+            state.loginError = message;
+        },
+        setRegisterError(state, message) {
+            state.registerError = message;
+        },
+        setList(state, list) {
+            state.list = list;
+        }
+    },
+    actions: {
+        // Register/Login
+        register(context, user) {
+            axios.post('/api/users', user).then(response => {
+                context.commit('setUser', response.data.user);
+                context.commit('setLogin', true);
+                context.commit('setLoginError', "");
+                context.commit('setRegisterError', "");
+            }).catch(error => {
+                context.commit('setLoginError', "");
+                context.commit('setLogin', false);
+                if (error.response) {
+                    if (error.response.status === 403) {
+                        context.commit('setRegisterError', "That username is already in use");
+                    }
+                    return;
+                }
+                context.commit('setRegisterError',"Sorry, your request failed. We will look into it.");
+            });
+        },
+        login(context, user) {
+            axios.post('/api/login', user).then(response => {
+                context.commit('setUser', response.data.user);
+                context.commit('setLogin', true);
+                context.commit('setLoginError', "");
+                context.commit('setRegisterError', "");
+            }).catch(error => {
+                context.commit('setRegisterError', "");
+                if (error.response) {
+                    if (error.response.status === 403 || error.response.status === 400)
+                      context.commit('setLoginError',"Invalid login.");
+                    context.commit('setRegisterError',"");
+                    return;
+                  }
+                  context.commit('setLoginError',"Sorry, your request failed. We will look into it.");              
+            });
+        },
+        logout(context,user) {
+            context.commit('setUser', {});
+            context.commit('setLogin',false);
+        },
+
+        // Items
+        getItems(context) {
+            axios.post('/api/users/' + context.state.user.id + '/items').then(response => {
+                context.commit('setList', response.data.items);
+            }).catch(err => {
+                console.log("getItems failed:",err);
+            });
+        },
+        addItem(context, item) {
+            axios.post('/api/users/' + context.state.user.id + '/items', item).then(response => {
+                return context.dispatch('getItems');
+            }).catch(err => {
+                console.log("addItem failed:",err);
+            });
+        }
+
+    }
+});
